9c7e6e97ed10d5164c6796bdc3de8d16756ce5cb
Robert Pelot <pelotrobert3@gmail.com>
2026-02-20 09:56:19 -0500

    moved to a new directory structure

---
+++ RegistrationProject/example.cs
@@ -0,0 +1,19 @@
+// for a server request:
+// Http://localhost:5001/api/movies/3 
+// GET POST PUT DELETE (method types)
+// optionally: a Body
+
+// Response:
+// status code (200, 400, 500, etc.)
+// a body (data, error message, etc.)
+//
+// status code 200 is good - 
+// 400 - not found maybe (bad request)
+// 401 - not authorized
+// 404 not found
+// 405 - method not allowed
+// 500 an exception on the server 
+// 503 - service unavailable
+// 201 - successful..
+
+// CRUD - Create, Read, Update, Delete
