c40fec1af74c55b4154b85a23e073381c46cafe5
Robert Pelot <pelotrobert3@gmail.com>
2026-03-27 23:00:42 -0400

    Completed post, get, update, and delete for courses using new repository

--- registrations-api/Repository/CourseRepository.cs
+++ registrations-api/Repository/CourseRepository.cs
@@ -1,165 +1,130 @@
 using System;
 using System.Collections.Generic;
 using CourseRegistration.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg;
+using Org.BouncyCastle.Tls.Crypto.Impl.BC;
 
 namespace CourseRegistration.Repository
 {
     public class CourseRepository
     {
-        public List<Course> Courses { get; set; }
-        public List<CoreGoal> Goals { get; set; }
-        public List<CourseOffering> Offerings { get; set; }
+        public IEnumerable<Course> AllCourses => GetAllCourses();
+
+        public Course GetCourseByName(string name) => GetCoursebyName(name);
+
+        private MySqlConnection _connection;
 
         public CourseRepository()
         {
-            Courses = new List<Course>();
-            Goals = new List<CoreGoal>();
-            Offerings = new List<CourseOffering>();
-
-            // Existing 4 courses
-            Course c1 = new Course()
-            {
-                Name = "ARTD 201",
-                Title = "Graphic Design",
-                Credits = 3.0,
-                Description = "Graphic design course"
-            };
-            Course c2 = new Course()
-            {
-                Name = "ARTS 101",
-                Title = "Art Studio",
-                Credits = 3.0,
-                Description = "Studio art course"
-            };
-            Course c3 = new Course()
-            {
-                Name = "STAT 201",
-                Title = "Statistics",
-                Credits = 4.0,
-                Description = "Intro to statistics"
-            };
-            Course c4 = new Course()
-            {
-                Name = "ENGL 302",
-                Title = "Math as a Communication Language",
-                Credits = 4.0,
-                Description = "Effective communication using quantitative concepts"
-            };
-
-            // 7 new courses
-            Course c5 = new Course()
-            {
-                Name = "CSCI 101",
-                Title = "Intro to Computer Science",
-                Credits = 3.0,
-                Description = "Fundamentals of programming"
-            };
-            Course c6 = new Course()
-            {
-                Name = "HIST 201",
-                Title = "World History",
-                Credits = 3.0,
-                Description = "Survey of global history"
-            };
-            Course c7 = new Course()
-            {
-                Name = "PHYS 101",
-                Title = "Physics I",
-                Credits = 4.0,
-                Description = "Mechanics and motion"
-            };
-            Course c8 = new Course()
-            {
-                Name = "CHEM 101",
-                Title = "Chemistry I",
-                Credits = 4.0,
-                Description = "Introductory chemistry"
-            };
-            Course c9 = new Course()
-            {
-                Name = "MATH 301",
-                Title = "Calculus II",
-                Credits = 4.0,
-                Description = "Advanced calculus"
-            };
-            Course c10 = new Course()
-            {
-                Name = "ENG 201",
-                Title = "English Literature",
-                Credits = 3.0,
-                Description = "Survey of literature"
-            };
-            Course c11 = new Course()
-            {
-                Name = "BIO 101",
-                Title = "Biology I",
-                Credits = 4.0,
-                Description = "Foundations of biology"
-            };
-
-            // Add all courses to repository
-            Courses.AddRange(new List<Course> { c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11 });
-
-            // Course Offerings
-            Offerings.Add(new CourseOffering() { TheCourse = c1, Section = "D1", Semester = "Spring 2021" });
-            Offerings.Add(new CourseOffering() { TheCourse = c3, Section = "01", Semester = "Spring 2021" });
-            Offerings.Add(new CourseOffering() { TheCourse = c2, Section = "01", Semester = "Spring 2022" });
-            Offerings.Add(new CourseOffering() { TheCourse = c5, Section = "01", Semester = "Fall 2021" });
-            Offerings.Add(new CourseOffering() { TheCourse = c6, Section = "02", Semester = "Spring 2022" });
-            Offerings.Add(new CourseOffering() { TheCourse = c7, Section = "01", Semester = "Fall 2022" });
-            Offerings.Add(new CourseOffering() { TheCourse = c8, Section = "01", Semester = "Spring 2021" });
-            Offerings.Add(new CourseOffering() { TheCourse = c9, Section = "02", Semester = "Fall 2021" });
-            Offerings.Add(new CourseOffering() { TheCourse = c10, Section = "01", Semester = "Fall 2022" });
-            Offerings.Add(new CourseOffering() { TheCourse = c11, Section = "01", Semester = "Spring 2022" });
-
-            // Core Goals
-            Goals.Add(new CoreGoal()
-            {
-                Id = "CG1",
-                Name = "Artistic Expression",
-                Description = "Desc for artistic expression",
-                Courses = new List<Course>() { c1, c2, c10, c11 } // Art, Studio, Literature
-            });
-            Goals.Add(new CoreGoal()
-            {
-                Id = "CG2",
-                Name = "Quantitative Literacy",
-                Description = "Desc for quantitative literacy",
-                Courses = new List<Course>() { c3, c5, c7, c9 } // Stats, CS, Physics, Calculus
-            });
-            Goals.Add(new CoreGoal()
-            {
-                Id = "CG3",
-                Name = "Effective Communication",
-                Description = "Desc for communication",
-                Courses = new List<Course>() { c4, c5, c6, c10, c11 } // Math/Comm, History, Biology
-            });
+            string connectionString = "server=localhost;user=csci330user;database=courseregistration;port=3306;password=csci330pass";
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();           
+        }
+        ~CourseRepository()
+        {
+            _connection.Close();
         }
 
-        public List<CourseOffering> getOfferingsByGoalIdAndSemester(string theGoalId, string semester)
+        // NEW METHOD BELOW: get all courses from the database
+        public IEnumerable<Course> GetAllCourses()
         {
-            CoreGoal theGoal = null;
-            foreach (CoreGoal cg in Goals)
+            var statement = "SELECT * FROM courses";
+            var command = new MySqlCommand(statement, _connection);
+            var reader = command.ExecuteReader();
+
+            List<Course> newList = new List<Course>(25);
+            while(reader.Read())
             {
-                if (cg.Id.Equals(theGoalId))
+                Course c = new Course
                 {
-                    theGoal = cg;
-                    break;
-                }
+                    Name = reader.GetString("name"),
+                    Title = reader.GetString("title"),
+                    Credits = reader.GetDouble("credits"),        
+                    Description = reader.GetString("description")
+                };
+                newList.Add(c);
             }
-            if (theGoal == null) throw new Exception("Didn't find the goal");
+            reader.Close();
+            return newList;
+        }
 
-            List<CourseOffering> courseOfferingsThatMeetGoal = new List<CourseOffering>();
+        // NEW METHOD BELOW: get a course by name from the database
+        public Course GetCoursebyName(string name)
+        {
+            var statement = "SELECT * FROM courses WHERE name = @name";
+            var command = new MySqlCommand(statement, _connection);
+            command.Parameters.AddWithValue("@name", name);
+            var reader = command.ExecuteReader();
 
-            foreach (CourseOffering c in Offerings)
+            Course c = null;
+            if(reader.Read())
             {
-                if (c.Semester.Equals(semester) && theGoal.Courses.Contains(c.TheCourse))
+                c = new Course
                 {
-                    courseOfferingsThatMeetGoal.Add(c);
-                }
+                    Name = reader.GetString("name"),
+                    Title = reader.GetString("title"),
+                    Credits = reader.GetDouble("credits"),        
+                    Description = reader.GetString("description")
+                };
             }
+            reader.Close();
+            return c;
+        }
 
-            return courseOfferingsThatMeetGoal;
+        // INSERT NEW COURSE:
+
+        public Course InsertCourse(Course newCourse)
+        {
+            var statement = "INSERT INTO courses (name, title, credits, description) VALUES (@newName, @newTitle, @newCredits, @newDescription)";
+            var command = new MySqlCommand(statement, _connection);
+            command.Parameters.AddWithValue("@newName", newCourse.Name);
+            command.Parameters.AddWithValue("@newTitle", newCourse.Title);
+            command.Parameters.AddWithValue("@newCredits", newCourse.Credits);
+            command.Parameters.AddWithValue("@newDescription", newCourse.Description);
+            int result = command.ExecuteNonQuery();
+            if (result == 1)
+            {
+                return newCourse;
+            } else
+            {
+                return null;
+            }
+        }
+
+        // Update a course in the database:
+
+        public bool UpdateCourse(Course course)
+        {
+            var statement = @"UPDATE courses
+                            SET title=@Title, credits=@Credits, description=@Description
+                            WHERE name=@Name";
+
+            using (var command = new MySqlCommand(statement, _connection))
+            {
+                command.Parameters.AddWithValue("@Title", course.Title);
+                command.Parameters.AddWithValue("@Credits", course.Credits);
+                command.Parameters.AddWithValue("@Description", course.Description);
+                command.Parameters.AddWithValue("@Name", course.Name);
+
+                int rowsAffected = command.ExecuteNonQuery(); // returns the number of rows updated
+                return rowsAffected > 0;
+            }
+        }
+
+        // Delete a course in the database:
+        public bool DeleteCourse(string name)
+        {
+            var statement = "DELETE FROM courses WHERE name = @Name";
+
+            using (var command = new MySqlCommand(statement, _connection))
+            {
+                command.Parameters.AddWithValue("@Name", name);
+
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
         }
     }
 }
--- registrations-api/Services/ICourseServices.cs
+++ registrations-api/Services/ICourseServices.cs
@@ -8,27 +8,11 @@ namespace CourseRegistration.Services
 {
     public interface ICourseServices
     {
-        List<CourseOffering> getOfferingsByGoalIdAndSemester(String theGoalId, String semester);
-        // get all courses:
-        List<Course> getCourses();
-        // get all course offerings for a given semester:
-        List<CourseOffering> getCourseOfferingsBySemester(string semester);
-        // get all course offerings for a given semester and department:
-        List<CourseOffering> getCourseOfferingsBySemesterAndDept(string semester, string dept);
-        // get all course offerings for a given goal id:
-        List<Course> getCoursesByGoalId(string goalId);
-        // get all course offerings for a given goal ids(more than 1):
-        List<Course> getCoursesByGoalIds(params string[] goalIds);
-        // get all core goals that are not covered by any course offerings in a given semester:
-        List<CoreGoal> getCoreGoalsThatAreNotCoveredBySemester(string semester);
-
-        /// new additions to option below:
-        Course getCourseByName(string name);
-        List<Course> searchCoursesByDepartment(string dept);
-        Course addCourse(Course course);
+        List<Course> GetAllCourses();       
+        Course GetCourseByName(string name);
+        Course AddCourse(Course newCourse);
         bool updateCourse(string name, Course updatedCourse);
-        bool deleteCourse(string name);
-        List<CoreGoal> getGoalsByCourseName(string name);
-        List<CourseOffering> getOfferingsByCourseNameAndSemester(string name, string semester);
+
+        bool DeleteCourse(string name);
     }   
 }
